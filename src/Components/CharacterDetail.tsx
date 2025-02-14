02d8a2ec2f6ec4cf18eedacb89bfd53a9805bb18
Kamil Wójtowicz <wojtowiczkamil02@gmail.com>
2025-02-14 20:57:40 +0100

    Finish detail page about character

--- src/Components/CharacterDetail.tsx
+++ src/Components/CharacterDetail.tsx
@@ -3,6 +3,14 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { ICharacter } from "../types";
 import { API_URL } from "../globals";
+import {
+  List,
+  Container,
+  Divider,
+  Grid,
+  GridColumn,
+  Image,
+} from "semantic-ui-react";
 
 const CharacterDetail = () => {
   const { id } = useParams<{ id: string }>();
@@ -14,7 +22,8 @@ const CharacterDetail = () => {
     setLoading(true);
     setError(null);
 
-    axios.get<ICharacter>(`${API_URL}${id}`)
+    axios
+      .get<ICharacter>(`${API_URL}${id}`)
       .then((response) => setCharacter(response.data))
       .catch((err) => {
         console.error("Error fetching character:", err);
@@ -23,18 +32,79 @@ const CharacterDetail = () => {
       .finally(() => setLoading(false));
   }, [id]);
 
+  const handleEmptyValues = (value: string | undefined) => {
+    if (value === undefined) return null;
+    return value.length != 0 ? value : <>?</>;
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
   return (
-    <div>
-      <h2>{character?.name}</h2>
-      <img src={character?.image} alt={character?.name} />
-      <p>Status: {character?.status}</p>
-      <p>Species:{character?.species}</p>
-      <p>Gender: {character?.gender}</p>
-    </div>
+    <Container>
+      <Divider />
+      <Grid>
+        <GridColumn width={4}>
+          <Image src={character?.image} />
+        </GridColumn>
+        <GridColumn width={6}>
+          <List size="medium">
+            <List.Item>
+              <strong>Id:</strong> {character?.id}
+            </List.Item>
+            <List.Item>
+              <strong>Name:</strong>
+              {handleEmptyValues(character?.name)}
+            </List.Item>
+            <List.Item>
+              <strong>Status:</strong> {handleEmptyValues(character?.status)}
+            </List.Item>
+            <List.Item>
+              <strong>Species:</strong> {handleEmptyValues(character?.species)}
+            </List.Item>
+            <List.Item>
+              <strong>Type:</strong> {handleEmptyValues(character?.type)}
+            </List.Item>
+            <List.Item>
+              <strong>Gender:</strong> {handleEmptyValues(character?.gender)}
+            </List.Item>
+            <List.Item>
+              <strong>Url:</strong> {handleEmptyValues(character?.url)}
+            </List.Item>
+            <List.Item>
+              <strong>Created:</strong> {handleEmptyValues(character?.created)}
+            </List.Item>
+            <List.Item>
+              <strong>Origin Name:</strong>
+              {handleEmptyValues(character?.origin.name)}
+            </List.Item>
+            <List.Item>
+              <strong>Origin Url:</strong>
+              {handleEmptyValues(character?.origin.url)}
+            </List.Item>
+            <List.Item>
+              <strong>Location Name:</strong>
+              {handleEmptyValues(character?.location.name)}
+            </List.Item>
+            <List.Item>
+              <strong>Location Url:</strong>
+              {handleEmptyValues(character?.location.url)}
+            </List.Item>
+          </List>
+        </GridColumn>
+      </Grid>
+      <h3>Episodes:</h3>
+      <List size="small">
+        <Grid columns={3}>
+          {character?.episode.map((ep, index) => (
+            <GridColumn>
+              <List.Item key={index}>{ep}</List.Item>
+            </GridColumn>
+          ))}
+        </Grid>
+      </List>
+    </Container>
   );
-  }
-  
-  export default CharacterDetail
+};
+
+export default CharacterDetail;
--- src/types.ts
+++ src/types.ts
@@ -3,8 +3,20 @@ export interface ICharacter {
   name: string;
   status: string;
   species: string;
+  type: string;
   gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
   image: string;
+  episode: string[];
+  url: string;
+  created: string;
 }
 
 export interface IApiResponse {
